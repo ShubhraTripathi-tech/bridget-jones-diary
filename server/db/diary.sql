410a5a42d8f633655367fb92c8ac52b1e2400aa1
Chi <whitneyotti@gmail.com>
2026-03-20 10:29:06 +0000

    Initial commit for database

---
+++ server/db/diary.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS entries
+
+CREATE TABLE entries (
+  post_id SERIAL PRIMARY KEY,
+  title TEXT,
+  content TEXT,
+  category TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO entries (title, content, category)
+VALUES 
+('Test Entry', 'My new diary app', 'Personal'),
